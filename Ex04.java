dd5055cb339bc299841a83d565e79595e94ab844
prathibhani <139643234+prathibhani@users.noreply.github.com>
2025-03-18 23:04:03 +0530

    Print the odd numbers between 1 to 20

---
+++ Ex04.java
@@ -0,0 +1,10 @@
+//print the odd numbers between 1 to 20
+
+public class Ex04{
+    public static void main(String args[]){
+
+        for(int i=1; i<=20; i+=2){
+            System.out.println(i);
+        }
+    }
+}
