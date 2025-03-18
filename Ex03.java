7380a611c683af3d98a9400c5e5f5fa7e6c006e7
prathibhani <139643234+prathibhani@users.noreply.github.com>
2025-03-18 22:56:28 +0530

    Print the numbers reverse

---
+++ Ex03.java
@@ -0,0 +1,12 @@
+//Print the numbers reverse
+class Ex03{
+	public static void main(String[] args){
+		int i=10;
+		while (i<11 && i>0){
+			System.out.println(i);
+            i=i-1;
+		}
+			
+	}
+	
+}
